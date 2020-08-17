8ee5649efb94bb9e9789b89f9ea7efe5f7d6f55a
Vitor Hugo <vhsilva.ap@gmail.com>
2020-08-17 13:05:22 -0300

    created sidebar options end start settings firebase in project

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANBKJeTdZ5Q46w92Dl59AELh0stVNZ5dw",
+    authDomain: "slack-clone-3932d.firebaseapp.com",
+    databaseURL: "https://slack-clone-3932d.firebaseio.com",
+    projectId: "slack-clone-3932d",
+    storageBucket: "slack-clone-3932d.appspot.com",
+    messagingSenderId: "977831074366",
+    appId: "1:977831074366:web:82a481625ccf41f26e8f6f",
+    measurementId: "G-5EBCNTX4ZF"
+};
